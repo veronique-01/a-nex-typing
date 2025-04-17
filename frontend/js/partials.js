44825a33759f3a1f194963c72e95064e00feb784
veronique-01 <veroniqueramasinarivo@gmail.com>
2025-04-17 16:48:51 +0200

    Fix: error in partial

--- frontend/js/partials.js
+++ frontend/js/partials.js
@@ -1,10 +1,18 @@
-fetch('../partials/header.html') 
+fetch('../partials/header.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
+
+
+        const script = document.createElement('script');
+        script.src = '../js/dropdown.js';
+        document.body.appendChild(script);
     });
-fetch('../partials/footer.html') 
+
+
+fetch('../partials/footer.html')
     .then(res => res.text())
     .then(data => {
-        document.getElementById('header').innerHTML = data;
+        document.getElementById('footer').innerHTML = data;
     });
+
